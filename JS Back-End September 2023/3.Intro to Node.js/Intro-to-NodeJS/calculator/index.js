89efe32688b4123970daecc2ba8416eee5e06472
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2023-09-13 21:45:07 +0300

    Delete JS Back-End September 2023/3.Intro to Node.js/Intro-to-NodeJS directory

--- JS Back-End September 2023/3.Intro to Node.js/Intro-to-NodeJS/calculator/index.js
+++
@@ -1,6 +0,0 @@
-const calculateDefaultExport = require("./calculator-default-export");
-const { sum } = require("./calculator-named-export");
-
-// console.log(calculateDefaultExport);
-// console.log(sum);
-// console.log(globalThis === global);
